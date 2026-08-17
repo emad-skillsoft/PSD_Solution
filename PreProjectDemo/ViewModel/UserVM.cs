94a0639a58ec47f272934684ddb55ff757802525
emad <emad.elfaramawi@skillsoft.com>
2026-08-17 18:28:19 +0300

    update login and users controller

---
+++ PreProjectDemo/ViewModel/UserVM.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PreProjectDemo.ViewModel
+{
+    public class UserVM
+    {
+        //public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+
+        public List<string> Roles { get; set; } = new();
+    }
+}
